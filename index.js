92c2446f9d6d88961b66166594af05ec96bd5fdb
joseph waine <josephwaine@josephs-iMac.local>
2020-03-27 12:11:17 -0700

    fix console error

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const port = process.env.PORT || 8888;
 const path = require('path')
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
-app.get('/about', (req, res) => res.sendFile(path.join(__dirname + '/public/about.html')));
-app.get('/yoga', (req, res) => res.sendFile(path.join(__dirname + '/public/yoga.html')));
-app.get('/kexp', (req, res) => res.sendFile(path.join(__dirname + '/public/kexp.html')));
-app.get('/soundcloud', (req, res) => res.sendFile(path.join(__dirname + '/public/soundcloud.html')));
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/public/contact.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/yoga', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/kexp', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/soundcloud', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
 
 
 app.use(express.static('public'));
