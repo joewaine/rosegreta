2d1a6a062d60b70352aa74b12b18bd9fad72629d
joseph waine <josephwaine@josephs-iMac.local>
2020-03-28 05:31:42 -0700

    greta push

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8888;
-const path = require('path')
+const path = require('path');
+const ejs = require('ejs');
+const csv2html = require('csv2html');
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
-app.get('/yoga', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
-app.get('/kexp', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
-app.get('/soundcloud', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
 
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+ }); 
+
+
+ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/views/index.ejs')));
+ app.get('/yoga', (req, res) => res.sendFile(path.join(__dirname + '/views/index.ejs')));
+ app.get('/kexp', (req, res) => res.sendFile(path.join(__dirname + '/views/index.ejs')));
+ app.get('/soundcloud', (req, res) => res.sendFile(path.join(__dirname + '/views/index.ejs')));
+ app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/views/index.ejs')));
 
 app.use(express.static('public'));
 
